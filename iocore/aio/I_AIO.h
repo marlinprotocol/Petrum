2f6be41578f87acbfc5e97c64e5ffcd6107147d2
cryptkit <cryptkit@marlin.pro>
2020-07-09 20:24:26 +0400

    async io

---
+++ iocore/aio/I_AIO.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include "tscore/ink_platform.h"
+#include "I_EventSystem.h"
+#include "records/I_RecProcess.h"
+
+#define AIO_MODULE_MAJOR_VERSION 1
+#define AIO_MODULE_MINOR_VERSION 0
+#define AIO_MODULE_VERSION makeModuleVersion(AIO_MODULE_MAJOR_VERSION, AIO_MODULE_MINOR_VERSION, PUBLIC_MODULE_HEADER)
+
+#define AIO_EVENT_DONE (AIO_EVENT_EVENTS_START + 0)
+
+#define AIO_MODE_THREAD 0
+#define AIO_MODE_NATIVE 1
+
+#if TS_USE_LINUX_NATIVE_AIO
+#define AIO_MODE AIO_MODE_NATIVE
+#else
+#define AIO_MODE AIO_MODE_THREAD
+#endif
+
+#define LIO_READ 0x1
+#define LIO_WRITE 0x2
+
+#if AIO_MODE == AIO_MODE_NATIVE
+
+#include <libaio.h>
+
+#define MAX_AIO_EVENTS 1024
+
+typedef struct iocb ink_aiocb;
+typedef struct io_event ink_io_event_t;
+
+// XXX hokey old-school compatibility with ink_aiocb.h ...
+#define aio_nbytes u.c.nbytes
+#define aio_offset u.c.offset
+#define aio_buf u.c.buf
+
+#else
+
+struct ink_aiocb {
+  int aio_fildes    = 0;
+  void *aio_buf     = nullptr; /* buffer location */
+  size_t aio_nbytes = 0;       /* length of transfer */
+  off_t aio_offset  = 0;       /* file offset */
+
+  int aio_reqprio    = 0; /* request priority offset */
+  int aio_lio_opcode = 0; /* listio operation */
+  int aio_state      = 0; /* state flag for List I/O */
+  int aio__pad[1];        /* extension padding */
+};
+
+bool ink_aio_thread_num_set(int thread_num);
+
+#endif
+
+// AIOCallback::thread special values
+#define AIO_CALLBACK_THREAD_ANY ((EThread *)0) // any regular event thread
+#define AIO_CALLBACK_THREAD_AIO ((EThread *)-1)
+
+#define AIO_LOWEST_PRIORITY 0
+#define AIO_DEFAULT_PRIORITY AIO_LOWEST_PRIORITY
+
+struct AIOCallback : public Continuation {
+  // set before calling aio_read/aio_write
+  ink_aiocb aiocb;
+  Action action;
+  EThread *thread   = AIO_CALLBACK_THREAD_ANY;
+  AIOCallback *then = nullptr;
+  // set on return from aio_read/aio_write
+  int64_t aio_result = 0;
+
+  int ok();
+  AIOCallback() {}
+};
+
+#if AIO_MODE == AIO_MODE_NATIVE
+
+struct AIOVec : public Continuation {
+  Action action;
+  int size;
+  int completed;
+  AIOCallback *first;
+
+  AIOVec(int sz, AIOCallback *c) : Continuation(new_ProxyMutex()), size(sz), completed(0), first(c)
+  {
+    action = c->action;
+    SET_HANDLER(&AIOVec::mainEvent);
+  }
+
+  int mainEvent(int event, Event *e);
+};
+
+struct DiskHandler : public Continuation {
+  Event *trigger_event;
+  io_context_t ctx;
+  ink_io_event_t events[MAX_AIO_EVENTS];
+  Que(AIOCallback, link) ready_list;
+  Que(AIOCallback, link) complete_list;
+  int startAIOEvent(int event, Event *e);
+  int mainAIOEvent(int event, Event *e);
+  DiskHandler()
+  {
+    SET_HANDLER(&DiskHandler::startAIOEvent);
+    memset(&ctx, 0, sizeof(ctx));
+    int ret = io_setup(MAX_AIO_EVENTS, &ctx);
+    if (ret < 0) {
+      Debug("aio", "io_setup error: %s (%d)", strerror(-ret), -ret);
+    }
+  }
+};
+#endif
+
+void ink_aio_init(ModuleVersion version);
+int ink_aio_start();
+void ink_aio_set_callback(Continuation *error_callback);
+
+int ink_aio_read(AIOCallback *op,
+                 int fromAPI = 0); // fromAPI is a boolean to indicate if this is from a API call such as upload proxy feature
+int ink_aio_write(AIOCallback *op, int fromAPI = 0);
+int ink_aio_readv(AIOCallback *op,
+                  int fromAPI = 0); // fromAPI is a boolean to indicate if this is from a API call such as upload proxy feature
+int ink_aio_writev(AIOCallback *op, int fromAPI = 0);
+AIOCallback *new_AIOCallback(void);
---
+++ iocore/aio/P_AIO.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include "P_EventSystem.h"
+#include "I_AIO.h"
+
+// for debugging
+// #define AIO_STATS 1
+
+#undef AIO_MODULE_VERSION
+#define AIO_MODULE_VERSION makeModuleVersion(AIO_MODULE_MAJOR_VERSION, AIO_MODULE_MINOR_VERSION, PRIVATE_MODULE_HEADER)
+
+TS_INLINE int
+AIOCallback::ok()
+{
+  return (off_t)aiocb.aio_nbytes == (off_t)aio_result;
+}
+
+extern Continuation *aio_err_callbck;
+
+#if AIO_MODE == AIO_MODE_NATIVE
+
+struct AIOCallbackInternal : public AIOCallback {
+  int io_complete(int event, void *data);
+  AIOCallbackInternal()
+  {
+    memset((void *)&(this->aiocb), 0, sizeof(this->aiocb));
+    SET_HANDLER(&AIOCallbackInternal::io_complete);
+  }
+};
+
+TS_INLINE int
+AIOVec::mainEvent(int /* event */, Event *)
+{
+  ++completed;
+  if (completed < size)
+    return EVENT_CONT;
+  else if (completed == size) {
+    SCOPED_MUTEX_LOCK(lock, action.mutex, this_ethread());
+    if (!action.cancelled)
+      action.continuation->handleEvent(AIO_EVENT_DONE, first);
+    delete this;
+    return EVENT_DONE;
+  }
+  ink_assert(!"AIOVec mainEvent err");
+  return EVENT_ERROR;
+}
+
+#else /* AIO_MODE != AIO_MODE_NATIVE */
+
+struct AIO_Reqs;
+
+struct AIOCallbackInternal : public AIOCallback {
+  AIO_Reqs *aio_req     = nullptr;
+  ink_hrtime sleep_time = 0;
+  SLINK(AIOCallbackInternal, alink); /* for AIO_Reqs::aio_temp_list */
+
+  int io_complete(int event, void *data);
+
+  AIOCallbackInternal()
+  {
+    aiocb.aio_reqprio = AIO_DEFAULT_PRIORITY;
+    SET_HANDLER(&AIOCallbackInternal::io_complete);
+  }
+};
+
+struct AIO_Reqs {
+  Que(AIOCallback, link) aio_todo;      /* queue for holding non-http requests */
+  Que(AIOCallback, link) http_aio_todo; /* queue for http requests */
+                                        /* Atomic list to temporarily hold the request if the
+                                           lock for a particular queue cannot be acquired */
+  ASLL(AIOCallbackInternal, alink) aio_temp_list;
+  ink_mutex aio_mutex;
+  ink_cond aio_cond;
+  int index           = 0; /* position of this struct in the aio_reqs array */
+  int pending         = 0; /* number of outstanding requests on the disk */
+  int queued          = 0; /* total number of aio_todo and http_todo requests */
+  int filedes         = 0; /* the file descriptor for the requests */
+  int requests_queued = 0;
+};
+
+#endif // AIO_MODE == AIO_MODE_NATIVE
+
+TS_INLINE int
+AIOCallbackInternal::io_complete(int event, void *data)
+{
+  (void)event;
+  (void)data;
+  if (aio_err_callbck && !ok()) {
+    AIOCallback *err_op          = new AIOCallbackInternal();
+    err_op->aiocb.aio_fildes     = this->aiocb.aio_fildes;
+    err_op->aiocb.aio_lio_opcode = this->aiocb.aio_lio_opcode;
+    err_op->mutex                = aio_err_callbck->mutex;
+    err_op->action               = aio_err_callbck;
+    eventProcessor.schedule_imm(err_op);
+  }
+  if (!action.cancelled) {
+    action.continuation->handleEvent(AIO_EVENT_DONE, this);
+  }
+  return EVENT_DONE;
+}
+
+#ifdef AIO_STATS
+class AIOTestData : public Continuation
+{
+public:
+  int num_req;
+  int num_temp;
+  int num_queue;
+  ink_hrtime start;
+
+  int ink_aio_stats(int event, void *data);
+
+  AIOTestData() : Continuation(new_ProxyMutex()), num_req(0), num_temp(0), num_queue(0)
+  {
+    start = ink_get_hrtime();
+    SET_HANDLER(&AIOTestData::ink_aio_stats);
+  }
+};
+#endif
+
+enum aio_stat_enum {
+  AIO_STAT_READ_PER_SEC,
+  AIO_STAT_KB_READ_PER_SEC,
+  AIO_STAT_WRITE_PER_SEC,
+  AIO_STAT_KB_WRITE_PER_SEC,
+  AIO_STAT_COUNT
+};
+extern RecRawStatBlock *aio_rsb;
